3f5a6655f42b711879907c80d3360670f0f2b0e0
choe5 <choe704110@gmail.com>
2026-04-11 01:57:24 +0800

    chore: remove TV project (moved to ArcFlow-TV-Launcher repo)

--- Downloads/Android_PJ/TV/app/src/main/java/com/choe/CoreTVLauncher/data/AppInfo.kt
+++
@@ -1,9 +0,0 @@
-package com.choe.CoreTVLauncher.data
-
-import android.graphics.drawable.Drawable
-
-data class AppInfo(
-    val packageName: String,
-    val label: String,
-    val icon: Drawable
-)
